d87957f2bef5c909a970db2e55f7624079a44376
kazlauskis <karolis@kazlauskis.com>
2016-02-05 16:54:41 +0000

    Add cordova build script

---
+++ pkgbuild.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo 'Build stared'
+
+#prepare www source
+npm install
+grunt init & grunt dev
+
+#init cordova source
+echo 'Initializing cordova'
+cordova create cordova com.ceh.irecord iRecord
+
+#add www source to cordova
+echo 'Moving dist to cordova'
+rm -R cordova/www/*
+cp -R dist/* cordova/www/ 
+
+cd cordova
+cordova platforms add ios android
+
+cordova plugin add org.apache.cordova.device
+cordova plugin add org.apache.cordova.console
+cordova plugin add org.apache.cordova.geolocation
+
+cordova build ios
