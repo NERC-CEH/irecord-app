d0bac29feeb545e8a243797694c0bdd2d2f78033
John van Breda <john.vanbreda@biodiverseit.co.uk>
2016-04-19 21:43:19 +0100

    Adds activities router to app.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,7 @@ import RecordsRouter from './components/records/router';
 import InfoRouter from './components/info/router';
 import SettingsRouter from './components/settings/router';
 import UserRouter from './components/user/router';
+import ActivitiesRouter from './components/activities/router';
 
 
 // Load the mighty app :)
