e7cdf97a2c07d88697ada6a44ffdb4821161284f
kazlauskis <karolis@kazlauskis.com>
2016-08-26 10:49:50 +0100

    Add grunt timer

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 var path = require('path');
 
 module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+
   require('load-grunt-config')(grunt, {
     // path to task.js files, defaults to grunt dir
     configPath: path.join(process.cwd(), 'config/build'),
