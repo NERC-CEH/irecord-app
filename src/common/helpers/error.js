b20b084d2bd5f19d35d1393ae19e6869aa9aa979
kazlauskis <karolis@kazlauskis.com>
2017-02-03 12:13:14 +0000

    Add array argument type to Error object

--- src/common/helpers/error.js
+++ src/common/helpers/error.js
@@ -1,6 +1,23 @@
+/** *********************************************************************
+ * ERROR
+ **********************************************************************/
 class Error {
-  constructor(message) {
-    this.message = message;
+  constructor(options = {}) {
+    if (typeof options === 'string') {
+      // message only
+      this.code = -1;
+      this.message = options;
+      return;
+    } else if (options instanceof Array) {
+      // array of errors
+      this.message = options.reduce(
+        (message, error) => `${message}${error.title}\n`,
+        '');
+      return;
+    }
+
+    this.code = options.code || -1;
+    this.message = options.message || '';
   }
 }
 
