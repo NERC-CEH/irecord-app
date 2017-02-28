63c501406aa26e344d2733ccb750df9a00736478
kazlauskis <karolis@kazlauskis.com>
2017-02-28 16:33:25 +0000

    Enable test helpers for sauce testing

--- test/karma.conf-sauce.js
+++ test/karma.conf-sauce.js
@@ -20,6 +20,7 @@ webpackConfigDev.plugins.splice(0, 0, new webpack.DefinePlugin({
     ENV: JSON.stringify(ENV),
   },
 }));
+webpackConfigDev.resolve.root.push(path.resolve('./test/'));
 
 const sauceBrowsers = _.reduce([
   ['firefox', '45'],
