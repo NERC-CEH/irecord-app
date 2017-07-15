a6da7f779a69840288dc41f9af698fcd69bef7bb
kazlauskis <karolis@kazlauskis.com>
2017-07-15 13:01:23 +0100

    Fix tests
    Signed-off-by: kazlauskis <karolis@kazlauskis.com>

--- src/common/helpers/__tests__/location-test.js
+++ src/common/helpers/__tests__/location-test.js
@@ -14,30 +14,30 @@ describe('Location', () => {
     expect(osCoords.x === 355000).to.be.true;
     expect(osCoords.y === 495000).to.be.true;
   });
-
-  it('should be mindful of location accuracy', () => {
-    let gridRef = location.locationToGrid({
-      latitude: 52.24394,
-      longitude: -3,
-      accuracy: 1,
-      source: 'gps',
-    });
-    expect(gridRef).to.equal('SO31816111');
-
-    gridRef = location.locationToGrid({
-      latitude: 52.24394,
-      longitude: -3,
-      accuracy: 5,
-      source: 'gps',
-    });
-    expect(gridRef).to.equal('SO318611');
-
-    gridRef = location.locationToGrid({
-      latitude: 51.153520339775575,
-      longitude: -3.2872083106005263,
-      accuracy: 80,
-      source: 'gps',
-    });
-    expect(gridRef).to.be.null;
-  });
+  // Users want to have more accurate GRs rather than correct ones
+  // it('should be mindful of location accuracy', () => {
+  //   let gridRef = location.locationToGrid({
+  //     latitude: 52.24394,
+  //     longitude: -3,
+  //     accuracy: 1,
+  //     source: 'gps',
+  //   });
+  //   expect(gridRef).to.equal('SO31816111');
+  //
+  //   gridRef = location.locationToGrid({
+  //     latitude: 52.24394,
+  //     longitude: -3,
+  //     accuracy: 5,
+  //     source: 'gps',
+  //   });
+  //   expect(gridRef).to.equal('SO318611');
+  //
+  //   gridRef = location.locationToGrid({
+  //     latitude: 51.153520339775575,
+  //     longitude: -3.2872083106005263,
+  //     accuracy: 80,
+  //     source: 'gps',
+  //   });
+  //   expect(gridRef).to.be.null;
+  // });
 });
