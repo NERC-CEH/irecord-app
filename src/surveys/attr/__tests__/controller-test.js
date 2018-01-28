77503f1f0495f4e2c9ca83ea9512be8c744dc636
kazlauskis <karolis@kazlauskis.com>
2018-01-28 14:50:22 +0000

    Clean up tests

--- src/surveys/attr/__tests__/controller-test.js
+++ src/surveys/attr/__tests__/controller-test.js
@@ -1,6 +1,6 @@
 import API from '../controller';
 
-describe('Surveys List Controller', () => {
+describe('Surveys Attr Controller', () => {
   it('should have a show method', () => {
     expect(API.show).to.be.a('function');
   });
