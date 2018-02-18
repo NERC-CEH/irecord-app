761f3a7c3f6bde7a7ddacad64429a8b59ca91d62
kazlauskis <karolis@kazlauskis.com>
2018-02-18 21:33:33 +0000

    Concat breadcrumbs

--- src/common/helpers/__tests__/analytics-test.js
+++ src/common/helpers/__tests__/analytics-test.js
@@ -1,6 +1,62 @@
 import Raven from 'raven-js';
-import API, { removeUserId, breadcrumbCallback } from '../analytics';
+import API, {
+  removeUserId,
+  breadcrumbCallback,
+  dataCallback,
+} from '../analytics';
 
+// a = [
+//   {
+//     timestamp: 1518983843.872,
+//     category: 'navigation',
+//     data: { to: '/#info', from: '/#samples' },
+//   },
+//   {
+//     timestamp: 1518983844.015,
+//     category: 'ui.click',
+//     message: 'div#left-panel.pull-left > a.icon.icon-left-nav',
+//   },
+//   {
+//     timestamp: 1518983844.019,
+//     message: 'Samples:List:Controller: showing 13.',
+//     level: 'debug',
+//     category: 'console',
+//   },
+//   {
+//     timestamp: 1518983845.561,
+//     category: 'ui.click',
+//     message: 'li.table-view-cell.swipe > a.mobile > div.media-body > div.core',
+//   },
+//   {
+//     timestamp: 1518983845.562,
+//     category: 'navigation',
+//     data: { to: '/#samples/UUID/edit', from: '/#samples' },
+//   },
+//   {
+//     timestamp: 1518983850.738,
+//     type: 'http',
+//     category: 'xhr',
+//     data: { url: 'https://www.brc.ac.uk/irecord/api/v1/samples' },
+//   },
+//   {
+//     timestamp: 1518983935.123,
+//     type: 'http',
+//     category: 'xhr',
+//     data: { url: 'https://www.brc.ac.uk/irecord/api/v1/users/USERID' },
+//   },
+//   {
+//     timestamp: 1518983935.126,
+//     message: 'Incorrect password or email.\n    ',
+//     level: 'error',
+//     category: 'console',
+//   },
+//
+//   {
+//     timestamp: 1518983844.015,
+//     category: 'ui.click',
+//     message: 'div#left-panel.pull-left > a.icon.icon-left-nav',
+//   },
+// ];
 describe('Helpers Analytics', () => {
   let RavenStub;
   before(() => {
@@ -13,11 +69,13 @@ describe('Helpers Analytics', () => {
 
   describe('breadcrumbCallback', () => {
     it('should remove userId from ajax URLs', () => {
-      API.init();
       const crumb = {
+        timestamp: 1518983935.123,
+        type: 'http',
         category: 'xhr',
         data: { url: 'https://www.brc.ac.uk/irecord/api/v1/users/ty%40uk2' },
       };
+
       const resultCrumb = breadcrumbCallback(crumb);
 
       expect(resultCrumb.data.url).to.equal(
@@ -27,6 +85,8 @@ describe('Helpers Analytics', () => {
 
     it('should not save image GETs', () => {
       const crumb = {
+        timestamp: 1518983969.273,
+        type: 'http',
         category: 'xhr',
         data: {
           method: 'GET',
@@ -34,7 +94,64 @@ describe('Helpers Analytics', () => {
         },
       };
       const resultCrumb = breadcrumbCallback(crumb);
-      expect(resultCrumb).to.be.false;
+      expect(resultCrumb.data.url).to.be.eql(
+        'file:///data/user/0/uk.ac.ceh.irecord/files/FILENAME.jpeg'
+      );
+    });
+  });
+
+  describe('dataCallback', () => {
+    it('should cap the breadcrumbs to max 100', () => {
+      const values = new Array(102).fill({});
+      const processed = dataCallback({ breadcrumbs: { values } });
+      expect(processed.breadcrumbs.values.length).to.be.equal(100);
+    });
+
+    it('should cap the breadcrumbs to last 100', () => {
+      const values = new Array(102).fill({});
+      values.push('a');
+      const processed = dataCallback({ breadcrumbs: { values } });
+      expect(processed.breadcrumbs.values[99]).to.be.equal('a');
+    });
+
+    it('should concat same xhr request crumbs', () => {
+      const values = [
+        {
+          timestamp: 1518983845.561,
+          category: 'ui.click',
+          message:
+            'li.table-view-cell.swipe > a.mobile > div.media-body > div.core',
+        },
+        {
+          timestamp: 1518983969.273,
+          type: 'http',
+          category: 'xhr',
+          data: {
+            method: 'GET',
+            url:
+              'file:///data/user/0/uk.ac.ceh.irecord/files/1471447312788.jpeg',
+          },
+        },
+        {
+          timestamp: 1518983971.273,
+          type: 'http',
+          category: 'xhr',
+          data: {
+            method: 'GET',
+            url:
+              'file:///data/user/0/uk.ac.ceh.irecord/files/1471447312789.jpeg',
+          },
+        },
+        {
+          timestamp: 1518983845.562,
+          category: 'navigation',
+          data: { to: '/#samples/UUID/edit', from: '/#samples' },
+        },
+      ];
+      const processed = dataCallback({ breadcrumbs: { values } });
+      expect(processed.breadcrumbs.values.length).to.be.equal(3);
+      expect(processed.breadcrumbs.values[1].data.url.includes('_x2')).to.be
+        .true;
     });
   });
 
--- src/common/helpers/analytics.js
+++ src/common/helpers/analytics.js
@@ -25,27 +25,79 @@ export function removeUserId(URL) {
 export function breadcrumbCallback(crumb) {
   // clean UUIDs
   if (crumb.category === 'navigation') {
-    const cleanCrumb = _.cloneDeep(crumb);
-    cleanCrumb.data = {
+    crumb.data = {
       to: removeUUID(crumb.data.to),
       from: removeUUID(crumb.data.from),
     };
-    return cleanCrumb;
+    return crumb;
   }
   if (crumb.category === 'xhr') {
     if (crumb.data.method === 'GET' && crumb.data.url.match(/jpeg$/i)) {
-      // do not log image requests
-      return false;
+      crumb.data.url = crumb.data.url.replace(
+        /files\/\d+\.jpeg/i,
+        'files/FILENAME.jpeg'
+      );
     }
 
-    const cleanCrumb = _.cloneDeep(crumb);
-    cleanCrumb.data = {
+    crumb.data = {
       url: removeUserId(crumb.data.url),
     };
-    return cleanCrumb;
+    return crumb;
   }
 
   return crumb;
+}
+
+function concatBreadcrumbs(breadcrumbs) {
+  const cleanBreadcrumbs = [];
+  let occurrences = 1;
+  breadcrumbs.forEach((crumb, i) => {
+    if (!cleanBreadcrumbs.length) {
+      cleanBreadcrumbs.push(crumb);
+      return;
+    }
+
+    const lastSavedCrumb = cleanBreadcrumbs[cleanBreadcrumbs.length - 1];
+    // count for duplicate crumbs
+    if (
+      lastSavedCrumb.category === 'xhr' &&
+      crumb.category === 'xhr' &&
+      lastSavedCrumb.data.method === crumb.data.method &&
+      lastSavedCrumb.data.url === crumb.data.url
+    ) {
+      occurrences++;
+      if (i === breadcrumbs.length - 1) {
+        lastSavedCrumb.data.url += `_x${occurrences}`;
+        occurrences = 1;
+      }
+      return;
+    }
+
+    // print out counter to last duplicate crumb
+    if (occurrences > 1) {
+      lastSavedCrumb.data.url += `_x${occurrences}`;
+      occurrences = 1;
+    }
+
+    cleanBreadcrumbs.push(crumb);
+  });
+
+  return cleanBreadcrumbs;
+}
+
+export function processBreadcrumbs(breadcrumbs) {
+  breadcrumbs.map(breadcrumbCallback);
+  return concatBreadcrumbs(breadcrumbs);
+}
+
+export function dataCallback(data) {
+  data.breadcrumbs.values = processBreadcrumbs(data.breadcrumbs.values);
+
+  // maxBreadcrumbs is 100 only, see the _globalOptions change below
+  const maxBreadcrumbs = 100;
+  const maxIndex = Math.max(data.breadcrumbs.values.length - maxBreadcrumbs, 0);
+  data.breadcrumbs.values.splice(0, maxIndex);
+  return data;
 }
 
 const API = {
@@ -72,9 +124,15 @@ const API = {
             'Incorrect password or email', // no need to log that
             'Backbone.history', // on refresh fires this error, todo: fix it
           ],
-          breadcrumbCallback,
+          // breadcrumbCallback, // moved to dataCallback
+          dataCallback,
         }
       ).install();
+
+      // increase breadcrumbs captured before send
+      // this is undocumented use of _globalOptions so might break in the future
+      Raven._globalOptions.maxBreadcrumbs = 400;
+      // console.log(Raven._globalOptions);
     } else {
       Log(
         'Analytics: server error logging is turned off. Please provide Sentry key.',
