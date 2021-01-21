5948971a0e3e55e97ba79d7b5ec7297391190f29
Richard Frost <richard.frost@headforwards.com>
2021-01-21 21:46:11 +0000

    Updated About page to be ERCCIS specific

--- src/Info/About/index.js
+++ src/Info/About/index.js
@@ -11,9 +11,25 @@ const Component = () => (
       <IonList lines="none">
         <IonItem>
           <IonLabel>
+            The ORKS app is a mobile application which enables you to record
+            wildlife sightings in Cornwall and Scilly on the go. You can log
+            your sighting with details, photos, descriptions and other
+            information, and upload this to the{' '}
+            <a href="https://www.orks.org.uk">www.ORKS.org.uk</a> website.
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>
             {t(
-              'iRecord App is an application that enables you to get involved in biological recording. You can contribute your sightings with photos, GPS acquired coordinates, descriptions and other information, thus providing scientists with important new biodiversity information that contributes to nature conservation, planning, research and education.'
+              'Your wildlife records are then available for conservation, sustainable planning, education and research work in Cornwall.'
             )}
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            ORKS website and app are developed and supported by ERCCIS,
+            Environmental Records Centre for Cornwall and the Isles of Scilly{' '}
+            <a href="https://www.erccis.org.uk">www.erccis.org.uk</a>
           </IonLabel>
         </IonItem>
       </IonList>
@@ -26,8 +42,57 @@ const Component = () => (
         <IonItem>
           <IonLabel>
             {t(
-              'We encourage everyone to get involved with recording species as it is very easy and quick to submit useful records without specialist knowledge. It doesn&apos;t matter whether you are an amateur enthusiast or a qualified biologist, iRecord App is for anyone who wants to contribute to our database observations of the natural environment.'
+              'We encourage everyone and anyone to use this app, to record and submit your wildlife sightings while out enjoying the Cornish wild places.'
             )}
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            {t(
+              'You don’t have to be an expert, this app is for anyone with an interest in wildlife and who want to do more to protect Cornwall’s Wildlife and Wild Places.'
+            )}
+          </IonLabel>
+        </IonItem>
+      </IonList>
+      <IonList lines="none">
+        <IonItem lines="inset">
+          <IonLabel>
+            <b>{t('How to use this app')}</b>
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            The ORKS app is linked to the Online Recording Kernow and Scilly
+            website - <a href="https://www.orks.org.uk">www.ORKS.org.uk</a>.
+            Your account and sightings submitted through the app can be viewed
+            and managed through the ORKS website.
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            Sign in or register an account on ORKS so you can submit your
+            records:
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            <ul>
+              <li>
+                - Add species sightings by going to the '+' icon at the bottom
+                of the app
+              </li>
+              <li>
+                - Add all the details for the sightings, and photos from your
+                device camera or gallery. The location of your sightings is
+                obtained through your phone GPS but can be updated using the
+                map.
+              </li>
+              <li>
+                - Once your record is correct and you've added all the details
+                you want it is ready to submit; click on the 'UPLOAD' button to
+                submit it to the ORKS website.
+              </li>
+            </ul>
           </IonLabel>
         </IonItem>
       </IonList>
@@ -39,20 +104,16 @@ const Component = () => (
         </IonItem>
         <IonItem>
           <IonLabel>
-            {t('This app was hand crafted with love by')}
-            <a href="https://flumens.io" style={{ whiteSpace: 'nowrap' }}>
-              {' '}
-              Flumens.
-            </a>
-            {' '}
             {t(
-              'Agency specializing in building bespoke data oriented sollutions.'
+              'This app was developed using open source code for the iRecord app. Developed by ERCCIS and funding by the Alexanda Fund for Recorders.'
             )}
-            {' '}
-            {t('For suggestions and feedback please do not hesitate to')}
-            {' '}
-            <a href="mailto:apps%40ceh.ac.uk?subject=iRecord%20App">
-              {t('contact us')}
+          </IonLabel>
+        </IonItem>
+        <IonItem>
+          <IonLabel>
+            If you have any suggestions of feedback please contact{' '}
+            <a href="mailto:erccis@cornwallwildlifetrust.org.uk?subject=ORKS%20App%20Support%20%26%20Feedback">
+              ERCCIS
             </a>
             .
           </IonLabel>
