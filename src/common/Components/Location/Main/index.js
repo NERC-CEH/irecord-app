b4d7d05e0ac710618ff818a6eab182587b8dbb60
Karolis Kazlauskis <karolis@flumens.io>
2021-07-30 12:09:42 +0100

    Fix OS map layer

--- src/common/Components/Location/Main/index.js
+++ src/common/Components/Location/Main/index.js
@@ -56,11 +56,13 @@ class AreaAttr extends Component {
     const isGPSTracking = this.props.sample.isGPSRunning();
 
     return (
-      <AppMain
-        id="location-page-content"
-        className={isGPSTracking ? 'gps-running' : ''}
-        ref={this.mapContainer}
-      />
+      <AppMain>
+        <div
+          id="location-page-content"
+          className={isGPSTracking ? 'gps-running' : ''}
+          ref={this.mapContainer}
+        />
+      </AppMain>
     );
   }
 }
