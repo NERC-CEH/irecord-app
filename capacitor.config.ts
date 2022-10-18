11e10d478e68c4440dabc2aef4fd484e17a9f988
Karolis Kazlauskis <karolis@flumens.io>
2022-10-18 11:56:09 +0300

    Refactor capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli'; // eslint-disable-line
+
+const config: CapacitorConfig = {
+  appId: 'uk.ac.ceh.irecord',
+  appName: 'iRecord App',
+  bundledWebRuntime: false,
+  webDir: 'build',
+  cordova: {},
+};
+
+export default config;
