4d59f163450104492e25cc6a1895842a383c94f4
Karolis Kazlauskis <karolis@flumens.io>
2022-10-31 17:42:03 +0200

    Fix status bar style

--- src/index.tsx
+++ src/index.tsx
@@ -49,7 +49,7 @@ async function init() {
 
   if (isPlatform('hybrid')) {
     StatusBar.setStyle({
-      style: StatusBarStyle.Dark,
+      style: StatusBarStyle.Light,
     });
 
     SplashScreen.hide();
