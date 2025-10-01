574cb31a42d97b4893bb0f70d7b7d7d83ee0a1e4
Karolis Kazlauskis <karolis@flumens.io>
2025-10-01 14:48:18 +0300

    Add missing translation terms [skip ci]

--- src/Settings/Menu/Main/index.tsx
+++ src/Settings/Menu/Main/index.tsx
@@ -26,13 +26,13 @@ function useDatabaseExportDialog(exportFn: any) {
     alert({
       header: 'Export',
       message: (
-        <>
+        <T>
           Are you sure you want to export the data?
           <p className="my-2 font-bold">
             This feature is intended solely for technical support and is not a
             supported method for exporting your data
           </p>
-        </>
+        </T>
       ),
       buttons: [
         {
